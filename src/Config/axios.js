7bd59be6f85f68149edca6ef5c940cbb7713be91
nihalDelimp <nihal@delimp.com>
2021-12-29 15:28:42 +0530

    config changes to static

--- src/Config/axios.js
+++ src/Config/axios.js
@@ -9,7 +9,7 @@ import { store } from '../Redux/store';
 export const authAxios = () => {
   let token = store.getState( 'state' ).auth.token;
   return axios.create( {
-    baseURL: process.env.REACT_APP_BASEURL,
+    baseURL: "https://api.delimp.world",
     headers: {
       Authorization: `${token ? `${token}` : null}`,
     },
@@ -18,7 +18,7 @@ export const authAxios = () => {
 
 export const withoutAuthAxios = () => {
   return axios.create( {
-    baseURL: process.env.REACT_APP_BASEURL,
+    baseURL: "https://api.delimp.world",
   } );
 };
 
