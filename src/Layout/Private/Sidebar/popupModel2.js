f26fc93cb85fec9768bda73765e90665c780098a
faizurdelimp <faizur@delimp.com>
2022-01-25 19:10:49 +0530

    changes in confirm model

--- src/Layout/Private/Sidebar/popupModel2.js
+++ src/Layout/Private/Sidebar/popupModel2.js
@@ -5,50 +5,53 @@ import IsLoadingHOC from "../../../Components/IsLoadingHOC"
 import { updateUsersProfile } from '../../../Redux/action/Auth'
 
 
-const PopUpModel2 = ( props ) => {
-    const { setLoading, SetModel2 , user_name , current_name , setCurrentName , profileData} = props
+const PopUpModel2 = (props) => {
+    const { setLoading, SetModel2, user_name, current_name, setCurrentName, profileData } = props
     const user_id = useSelector(state => state.auth.user.id)
-   
-    const [checked, setChecked] = useState( false )
+
+    const [checked, setChecked] = useState(false)
     const dispatch = useDispatch()
     const handlerChecked = event => {
-        if ( event.target.checked ) {
-            setChecked( true )
+        if (event.target.checked) {
+            setChecked(true)
         } else {
-            setChecked( false )
+            setChecked(false)
         }
     }
 
     const changeHandler = async () => {
-        if(current_name.length < 5){
+        if (current_name.length < 5) {
             toast.error("Name must be at least 5 chars long")
         }
-        else{
-        setLoading(true)
-        await dispatch(updateUsersProfile( { name : current_name }))
-            .then(
-                response => {
-                    setLoading(false)
-                    toast.success(response.message)
-                    SetModel2( false )
-                    profileData();
-               
-                },
-                error => {
-                    toast.error(error.response.data.message)
-                    setLoading(false)
-                }
-            )
-            .catch(
-                error => console.log(error)
-            )
+        else {
+            setLoading(true)
+            await dispatch(updateUsersProfile({ name: current_name }))
+                .then(
+                    response => {
+                        setLoading(false)
+                        toast.success(response.message)
+                        SetModel2(false)
+                        profileData();
+
+                    },
+                    error => {
+                        toast.error(error.response.data.message)
+                        setLoading(false)
+                    }
+                )
+                .catch(
+                    error => console.log(error)
+                )
         }
     }
 
 
     return (
-        <div className="popup" onClick={() => SetModel2( false )}>
-            <div className="popup--card" onClick={( e ) => e.stopPropagation()}>
+        <div className="popup" onClick={() => {
+            setCurrentName(user_name)
+            SetModel2(false)
+        }}>
+            <div className="popup--card" onClick={(e) => e.stopPropagation()}>
                 <h3>Please Confirm ?</h3>
                 <div className="form-check">
                     <input
@@ -60,14 +63,15 @@ const PopUpModel2 = ( props ) => {
                     <label className="form-check-label" htmlFor="exampleCheck1">Are you sure you want to change your name ?</label>
                 </div>
                 <div className="btn--group">
-                    <button className="update"  onClick={changeHandler}  disabled={!checked}>Update</button>
-                    <button className="cancel" onClick={() =>{
-                     setCurrentName(user_name)
-                     SetModel2( false )}}>Cancel</button>
+                    <button className="update" onClick={changeHandler} disabled={!checked}>Update</button>
+                    <button className="cancel" onClick={() => {
+                        setCurrentName(user_name)
+                        SetModel2(false)
+                    }}>Cancel</button>
                 </div>
             </div>
         </div>
     )
 }
 
-export default IsLoadingHOC( PopUpModel2 )
+export default IsLoadingHOC(PopUpModel2)
